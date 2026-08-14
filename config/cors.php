c3e818624117e7f109eea9cc283a10d45fdb23ca
Marcos Marinho <marcosmarinho@santos.sp.gov.br>
2026-08-14 17:26:55 -0300

    feat cors backend para vercel

--- config/cors.php
+++ config/cors.php
@@ -5,6 +5,9 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://localhost:3000',
+        'https://maremu-l2cr.vercel.app',
+        'https://maremu.com.br',
+        'https://www.maremu.com.br',
         env('FRONTEND_URL'),
     ],
     'allowed_headers' => ['*'],
